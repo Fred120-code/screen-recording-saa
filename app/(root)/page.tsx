5619f31c1d6ace6192e57cebcafabd65d988dda9
Fred120-code <fredayemtsa@gmail.com>
2026-09-24 02:03:08 +0100

    feat: add navbar and header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Page = () => {
+  return (
+    <div className="wrapper page">
+      <Header subHeader="Public Library" title="All Vidéo" />
+      <h1 className="text-2xl font-karla"> welcome to the new project</h1>
+    </div>
+  );
+};
+
+export default Page;
